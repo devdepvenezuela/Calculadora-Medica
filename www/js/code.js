9ebad5b23d59c52417acd84e276ddf5fb4459959
devdepvenezuela <devdepvenezuela@gmail.com>
2015-03-30 16:19:26 -0430

    Update code.js

--- www/js/code.js
+++ www/js/code.js
@@ -6,7 +6,7 @@ $(function(){
 })
 var swiperParent = new Swiper('.swiper-parent',{
 pagination: '.pagination',
-paginationClickable: false,
+paginationClickable: true,
 onSlideChangeEnd : function() {
   //Do something when you touch the slide
   if (swiperParent.activeIndex != 0){
